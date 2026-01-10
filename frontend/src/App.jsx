adc115538cdc37cf16fdd9d5864ea25fbb8da9e6
Shrishkd <shrishdas444@gmail.com>
2026-01-10 16:27:23 +0530

    frontend modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
     try {
       const res = await axios.get(
-        `https://growstocks-backend.onrender.com/predict=${stock}`
+        `http://127.0.0.1:8000/predict?stock=${stock}`
       );
       setResult(res.data);
     } catch (err) {
